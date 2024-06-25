16c520815e9bebc06518af9d1a88ef11daeb1107
Kotaro <swdevstudio@gmail.com>
2024-06-25 22:59:07 +0500

    SOW-15: Add support for stylelint in VS Code.

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [
-    vue(),
-    stylelint({
-      fix: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
+export default defineConfig(async ({ mode }) => {
+  const env = loadEnv('', process.cwd())
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+  return {
+    plugins: [
+      vue(),
+      stylelint({
+        fix: env.VITE_STYLELINT_AUTO_FIX === 'true',
+        dev: mode === 'development'
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     },
-  },
-}));
+
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+      port: 1420,
+      strictPort: true,
+      watch: {
+        // 3. tell vite to ignore watching `src-tauri`
+        ignored: ["**/src-tauri/**"],
+      },
+    },
+  }
+});
