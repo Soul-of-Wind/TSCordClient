da34bc476f09eed11a6b3e83eac48635576fc300
Kotaro <swdevstudio@gmail.com>
2024-06-26 23:19:40 +0500

    SOW-13: Install and tune eslint

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+import pluginVue from 'eslint-plugin-vue';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...pluginVue.configs['flat/recommended'],
+    {
+        ignores: ['*.config.{ts,js}'],
+        languageOptions: {
+            parserOptions: {
+                // parser: tseslint.parser,
+                // project: true,
+                // tsconfigRootDir: import.meta.dirname,
+                // extraFileExtensions: ['.vue'],
+                parser: tseslint.parser,
+                project: './tsconfig.json',
+                extraFileExtensions: ['.vue'],
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            semi: 'error',
+            "@typescript-eslint/naming-convention": "error",
+            "@typescript-eslint/no-explicit-any": "error",
+            quotes: ['error', 'single'],
+        }
+
+    },
+    {
+        ignores: ['**/vite-env.d.ts']
+    },
+);
+
