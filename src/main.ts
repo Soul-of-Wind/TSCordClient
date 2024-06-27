b48a7154a58471562dc4321bdbd98e47035505e9
Kotaro <swdevstudio@gmail.com>
2024-06-28 00:31:38 +0500

    SOW-18: modification of i18n
    Fixed some bugs, replaced the file from index.js to index.ts, corrected writing variables using ESlint

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-import { createI18n, useI18n } from 'vue-i18n';
+import i18n from '@/assets/i18n';
 
-import EN from './i18n/en.json'
-import RU from './i18n/ru.json'
-
-// use $t('title')
-const i18n = createI18n({
-    locale: 'RU',
-    messages: {
-        EN: EN,
-        RU: RU,
-    }
-})
-
-const pinia = createPinia()
+const pinia = createPinia();
 
 createApp(App)
     .use(router)
