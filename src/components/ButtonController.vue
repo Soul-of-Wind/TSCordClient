b52e65a72b752f3e255ece6165d762e5682ba193
Luffy <ponosonik1@mail.ru>
2024-07-01 21:04:26 +0500

    SOW-12: Add jest

---
+++ src/components/ButtonController.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="button-controller">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: "ButtonController"
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/jest/vue-component.test.ts
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/vue";
+import ButtonController from "@ButtonController.vue"
+
+
+// function test('renderers base button', () => {
+//     const { debug } = render(ButtonController)
+
+//     debug()
+// })
